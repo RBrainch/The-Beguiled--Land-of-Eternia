dba2400da102253216c974daaf3d80ac6a06291e
CClegg66 <christianclegg66@gmail.com>
2024-07-17 10:31:39 -0400

    removed treasure rooms and made player better

--- The Beguiled Land of Eternia/Assets/Scripts/PlayerControllerChristian.cs
+++ The Beguiled Land of Eternia/Assets/Scripts/PlayerControllerChristian.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerControllerChristian : MonoBehaviour
 {
+    public GameObject Camera;
     public float MoveSpeed = 2.5f;
+    private Rigidbody2D RigidBody;
 
     //public Spell sampleSpell;
     public GameObject spellImage1;
@@ -14,25 +16,26 @@ public class PlayerControllerChristian : MonoBehaviour
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        RigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Camera.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+
         float dx = Input.GetAxisRaw("Horizontal");
         float dy = Input.GetAxisRaw("Vertical");
-        
 
-        
         if (dx != 0 && dy != 0)
         {
             dx /= Mathf.Sqrt(2);
             dy /= Mathf.Sqrt(2);
         }
-        dx *= Time.deltaTime * MoveSpeed;
-        dy *= Time.deltaTime * MoveSpeed;
+        dx *= MoveSpeed;
+        dy *= MoveSpeed;
 
-        transform.Translate(dx, dy, 0);
+        RigidBody.velocity = new Vector2(dx, dy);
         if (dx > 0) {
             sr.flipX = true;
         }
