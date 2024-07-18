12e8d57475966f27829ae05a909e623b935cf435
RBrainch <rishaansbrainch@gmail.com>
2024-07-18 11:22:34 -0400

    fitlescreen

--- The Beguiled Land of Eternia/Assets/Scripts/ButtonManager.cs
+++ The Beguiled Land of Eternia/Assets/Scripts/ButtonManager.cs
@@ -1,8 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    public GameObject titleCanvas;
+    public GameObject controlsCanvas;
+    public GameObject backgroundCanvas;
+   
+   public void StartGame() {
+        SceneManager.LoadScene(1);
+
+   }
+   public void Controls() {
+    titleCanvas.SetActive(false);
+    controlsCanvas.SetActive(true);
+     backgroundCanvas.SetActive(false);
+
+   }
+   public void TitleView() {
+    backgroundCanvas.SetActive(true);
+    titleCanvas.SetActive(true);
+    controlsCanvas.SetActive(false);
+    
+   }
+   
+   
     // Start is called before the first frame update
 }
+
